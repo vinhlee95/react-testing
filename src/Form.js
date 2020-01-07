6b019e987253139691ed24a30f852ce11c67af56
Vinh Le <vinh@vertics.co>
2020-01-07 22:54:46 +0200

    feat: add form accessibility test

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form() {
+	return(
+		<form>
+			<label htmlFor='form-input'>Hello world label</label>
+			<input id='form-input' placeholder='hello' />
+		</form>
+	)
+}
+
+export default Form
