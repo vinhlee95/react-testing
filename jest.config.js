2794310de64c05051e5912fb1a9a191e35879cfb
Vinh Le <vinh@vertics.co>
2020-01-07 22:54:33 +0200

    feat: add jest-axe extend expect for testing accessibility

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
 	testEnvironment: 'jest-environment-jsdom',
-	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', 'jest-axe/extend-expect'],
   moduleDirectories: [
     'node_modules',
     path.join(__dirname, '../src'),
