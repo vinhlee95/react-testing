fb4790dcffea5e82942627e2f7f592e0a2a52e02
Vinh Le <vinh@vertics.co>
2020-02-12 06:58:24 +0200

    feat: add test for submitting the button

--- src/__tests__/tdd-form.js
+++ src/__tests__/tdd-form.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import {TddForm} from '../components/TddForm'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 
 test('should render a form with title, content and submit button', () => {
   const {getByLabelText, getByText} = render(<TddForm />)
   getByLabelText(/title/i)
   getByLabelText(/content/i)
-  getByText(/submit/i)
+  const submitButton = getByText(/submit/i)
+
+  fireEvent.click(submitButton)
+
+  expect(submitButton).toBeDisabled()
 })
--- src/components/TddForm.js
+++ src/components/TddForm.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 export const TddForm = () => {
+  const [isSubmitting, setSubmitting] = React.useState(false)
+
+  const onSubmit = e => {
+    e.preventDefault()
+    setSubmitting(true)
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <label htmlFor='title-input'>Title</label>
       <input id='title-input' />
       <label htmlFor='content-input'>Content</label>
       <input id='content-input' />
-      <button>Submit</button>
+      <button type='submit' disabled={isSubmitting}>Submit</button>
     </form>
   )
 }
